995abd6917171d0fb044137e4d98ae1b9a1fc85f
Raffaella_SF <adraffaella6@gmail.com>
2025-11-18 11:20:11 -0400

    add: distinção entre database local e servidor

--- backend/app/core.py
+++ backend/app/core.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL_LOCAL: str
+    DATABASE_URL_SERVER: str
     GITHUB_API_URL: str
     SECRET_KEY: str
     ALGORITHM: str
